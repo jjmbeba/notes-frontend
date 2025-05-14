e70f38ab118de77c6d07b543ea9bddb86bdfb9bb
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-05-14 18:14:45 +0300

    add t3-env for typesafe env variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { EditIcon } from "lucide-react"
-import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card"
 import { Button } from "./components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card"
 import { Checkbox } from "./components/ui/checkbox"
 
 function App() {
